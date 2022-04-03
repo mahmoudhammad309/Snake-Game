09c1bb39a07eea1c7acf1175eb7435f6ce5a3c57
Mahmoud Hammad <0597694093mahmoud@gmail.com>
2022-04-03 21:23:28 +0300

    modify the endpoint reatles #12

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ const gameBoard = document.getElementById("game-board");
 function main(currentTime) {
   if (gameOver) {
     if (confirm("You Lost, Press Ok to restart")) {
-      window.location = "/";
+      window.location = "/Snake-Game/";
     }
     return;
   }
